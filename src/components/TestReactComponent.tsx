6b39f2a2083224936af03821d735add61ecc9b1d
FUGAMARU <gofugashi@gmail.com>
2024-06-01 13:46:26 +0900

    chore: VSCode設定ファイル作成

--- src/components/TestReactComponent.tsx
+++ src/components/TestReactComponent.tsx
@@ -1,11 +1,11 @@
 type Props = {
-  arr: Array<string>;
-};
+  arr: Array<string>
+}
 
 export const TestReactComponent = () => {
   return (
     <div>
       <h1>Test React Component</h1>
     </div>
-  );
-};
+  )
+}
