718b1768204a5a9f008d26acec6072fc4a158bfd
FUGAMARU <gofugashi@gmail.com>
2024-06-09 23:32:12 +0900

    refactor: ItemNumberのデザイン変更

--- src/components/common/ItemNumber.stories.tsx
+++ src/components/common/ItemNumber.stories.tsx
@@ -5,17 +5,34 @@ import type { Meta, StoryObj } from "@storybook/react"
 const meta: Meta<typeof ItemNumber> = {
   component: ItemNumber,
   tags: ["autodocs"],
-  args: { itemNumber: undefined, isOutlined: undefined },
-  argTypes: {
-    itemNumber: { control: "text" },
-    isOutlined: { control: "boolean" }
-  }
+  args: { itemNumber: undefined, isFilled: undefined },
+  argTypes: { itemNumber: { control: "text" }, isFilled: { control: "boolean" } }
 }
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-/** 初期表示 */
-export const Primary: Story = {
+/** Outlined */
+export const Outlined: Story = {
   args: { itemNumber: "1" }
+}
+
+/** Filled */
+export const Filled: Story = {
+  args: { itemNumber: "1", isFilled: true }
+}
+
+/** 2桁 */
+export const DoubleDigit: Story = {
+  args: { itemNumber: "99" }
+}
+
+/** 3桁以上 */
+export const MultiDigit: Story = {
+  args: { itemNumber: "100" }
+}
+
+/** ハイフンあり */
+export const WithHyphen: Story = {
+  args: { itemNumber: "1-1" }
 }
--- src/components/common/ItemNumber.tsx
+++ src/components/common/ItemNumber.tsx
@@ -7,23 +7,36 @@ type Props = {
   /** 項番 */
   itemNumber: string
   /** アウトライン表示かどうか */
-  isOutlined?: boolean
+  isFilled?: boolean
 }
 
 /**
  * 項番
  * @returns ReactNode
  */
-const ItemNumber = ({ itemNumber, isOutlined = false }: Props): ReactNode => {
-  /** 項番が1文字かどうか */
-  const isSingleDigit = useMemo(() => itemNumber.length === 1, [itemNumber])
+const ItemNumber = ({ itemNumber, isFilled = false }: Props): ReactNode => {
+  /** ハイフンを含むかどうか */
+  const includesHyphen = useMemo(() => itemNumber.includes("-"), [itemNumber])
+
+  /** 1文字かどうか */
+  const isOneCharacter = useMemo(() => itemNumber.length === 1, [itemNumber])
+
+  /** 2文字かどうか */
+  // eslint-disable-next-line no-magic-numbers
+  const isTwoCharacters = useMemo(() => itemNumber.length === 2, [itemNumber])
+
+  /** 3文字かどうか */
+  // eslint-disable-next-line no-magic-numbers
+  const isThreeCharacters = useMemo(() => itemNumber.length === 3, [itemNumber])
 
   return (
     <div
       className={clsx(
         styles.itemNumber,
-        isSingleDigit && styles.SizeFixed,
-        isOutlined && styles.Outlined
+        isOneCharacter && styles.OneCharacter,
+        isTwoCharacters && styles.TwoCharacters,
+        isThreeCharacters && !includesHyphen && styles.ThreeCharacters,
+        isFilled && styles.Filled
       )}
     >
       {itemNumber}
