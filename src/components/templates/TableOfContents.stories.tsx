6eefc80f79cefe361867527db5548dfe62bcf6ef
FUGAMARU <gofugashi@gmail.com>
2024-06-11 00:42:53 +0900

    feat: TableOfContentsのアクティブ表示の出し分けを追加

--- src/components/templates/TableOfContents.stories.tsx
+++ src/components/templates/TableOfContents.stories.tsx
@@ -5,8 +5,8 @@ import type { Meta, StoryObj } from "@storybook/react"
 const meta: Meta<typeof TableOfContents> = {
   component: TableOfContents,
   tags: ["autodocs"],
-  args: { contents: undefined },
-  argTypes: {}
+  args: { contents: undefined, activeItemHref: undefined },
+  argTypes: { activeItemHref: { control: "text" } }
 }
 
 export default meta
@@ -36,44 +36,45 @@ export const Primary: Story = {
       },
       {
         h2: {
-          title: "見出し1",
-          href: "#heading1",
+          title: "見出し2",
+          href: "#heading2",
           h3: [
             {
-              title: "見出し1-1",
-              href: "#heading1-1",
+              title: "見出し2-1",
+              href: "#heading2-1",
               h4: [
                 {
-                  title: "見出し1-1-1",
-                  href: "#heading1-1-1"
+                  title: "見出し2-1-1",
+                  href: "#heading2-1-1"
                 }
               ]
             },
             {
-              title: "見出し1-2",
-              href: "#heading1-2"
+              title: "見出し2-2",
+              href: "#heading2-2"
             }
           ]
         }
       },
       {
         h2: {
-          title: "見出し1",
-          href: "#heading1",
+          title: "見出し3",
+          href: "#heading3",
           h3: [
             {
-              title: "見出し1-1",
-              href: "#heading1-1"
+              title: "見出し3-1",
+              href: "#heading3-1"
             }
           ]
         }
       },
       {
         h2: {
-          title: "見出し1",
-          href: "#heading1"
+          title: "見出し4",
+          href: "#heading4"
         }
       }
-    ]
+    ],
+    activeItemHref: "#heading2"
   }
 }
--- src/components/templates/TableOfContents.tsx
+++ src/components/templates/TableOfContents.tsx
@@ -6,7 +6,7 @@ import TableOfContentsItem from "@/components/templates/TableOfContentsItem"
 
 type Props = {
   contents: Array<{
-    h2?: {
+    h2: {
       title: string
       href: string
       h3?: Array<{
@@ -19,32 +19,32 @@ type Props = {
       }>
     }
   }>
+  /** アクティブ表示にするアイテムのhref */
+  activeItemHref: string
 }
 
 /**
  * 目次
  * @returns ReactNode
  */
-const TableOfContents = ({ contents }: Props): ReactNode => {
+const TableOfContents = ({ contents, activeItemHref }: Props): ReactNode => {
   return (
     <div className={styles.tableOfContents}>
       <div className={styles.main}>
         {contents.map((content, h2Index) => (
-          <Fragment key={content.h2?.title}>
-            {content.h2 !== undefined && (
-              <TableOfContentsItem
-                href={content.h2.href}
-                isActive={false}
-                itemNumber={`${h2Index + 1}`}
-                title={content.h2.title}
-              />
-            )}
-            {content.h2?.h3?.map((h3, h3Index) => (
+          <Fragment key={content.h2.title}>
+            <TableOfContentsItem
+              href={content.h2.href}
+              isActive={activeItemHref === content.h2.href}
+              itemNumber={`${h2Index + 1}`}
+              title={content.h2.title}
+            />
+            {content.h2.h3?.map((h3, h3Index) => (
               <Fragment key={h3.title}>
                 <div className={styles.headingH3}>
                   <TableOfContentsItem
                     href={h3.href}
-                    isActive={false}
+                    isActive={activeItemHref === h3.href}
                     itemNumber={`${h2Index + 1}-${h3Index + 1}`}
                     title={h3.title}
                   />
@@ -53,7 +53,7 @@ const TableOfContents = ({ contents }: Props): ReactNode => {
                   <div key={h4.title} className={styles.headingH4}>
                     <TableOfContentsItem
                       href={h4.href}
-                      isActive={false}
+                      isActive={activeItemHref === h4.href}
                       itemNumber={`${h2Index + 1}-${h3Index + 1}-${h4Index + 1}`}
                       title={h4.title}
                     />
