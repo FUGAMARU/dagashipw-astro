61d9abd5a989937fc02258a96fa87cd1a1c06306
FUGAMARU <gofugashi@gmail.com>
2024-07-02 23:41:48 +0900

    refactor: 記事ページ以外ではサイドバーに目次が表示されないように

--- src/constants/value.ts
+++ src/constants/value.ts
@@ -8,3 +8,6 @@ export const API_BASE_URL = `${STRAPI_BASE_URL}/api`
 
 /** sessionStorageに記録する目次データーのキー */
 export const SESSION_STORAGE_TABLE_OF_CONTENTS_KEY = "table-of-contents"
+
+/** PC版目次のコンテナ要素のID */
+export const PC_TABLE_OF_CONTENTS_CONTAINER_ID = "table-of-contents-pc"
