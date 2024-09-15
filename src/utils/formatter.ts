b9bc252fa2e591448b165d7f24e7f67f7f39e363
FUGAMARU <gofugashi@gmail.com>
2024-09-16 00:02:13 +0900

    refactor: formatter整理

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -14,44 +14,6 @@ import type { ArticleInfo } from "@/types/article"
 import type { components } from "@/types/schema"
 
 /**
- * 記事のMarkdownテキストから冒頭の段落を抽出し、所定の文字数だけ切り取る
- *
- * @param markdown - Markdown
- * @returns 所定の文字数で切り取られた段落
- */
-export const extractBeginningParagraph = (markdown: string): string => {
-  const paragraphs = markdown
-    // Markdownリンクのテキスト部分だけを抽出
-    .replace(/\[([^\]]+)]\([^)]+\)/g, "$1")
-    // Markdown画像を削除
-    .replace(/!\[[^\]]*]\([^)]+\)/g, "")
-    // HTMLタグを削除し、タグ内のテキストを保持
-    .replace(/<\/?[^>]+(>|$)/g, "")
-    // 見出しなどの余計なものを削除
-    .replace(/(^|\n)(#+\s|<[^>]+>)/g, "")
-    // 改行をスペースに変換
-    .replace(/\n+/g, " ")
-    // 不要なスペースをトリム
-    .trim()
-
-  return paragraphs.substring(0, EXTRACTED_PARAGRAPHS_LENGTH)
-}
-
-/**
- * 日付文字列をyyyy/MM/dd形式に整形する
- *
- * @param dateString - 日付文字列
- * @returns yyyyMM/dd
- */
-export const formatDateString = (dateString: string): string => {
-  const date = new Date(dateString)
-  const year = date.getFullYear()
-  const month = `0${date.getMonth() + 1}`.slice(-2)
-  const day = `0${date.getDate()}`.slice(-2)
-  return `${year}/${month}/${day}`
-}
-
-/**
  * CMSから取得した記事情報を内部的に扱いやすいフォーマットに整形する
  *
  * @param articleInfo - 記事情報
@@ -60,6 +22,44 @@ export const formatDateString = (dateString: string): string => {
 export const formatArticleInfo = async (
   articleInfo: components["schemas"]["Article"]
 ): Promise<ArticleInfo> => {
+  /**
+   * 記事のMarkdownテキストから冒頭の段落を抽出し、所定の文字数だけ切り取る
+   *
+   * @param markdown - Markdown
+   * @returns 所定の文字数で切り取られた段落
+   */
+  const extractBeginningParagraph = (markdown: string): string => {
+    const paragraphs = markdown
+      // Markdownリンクのテキスト部分だけを抽出
+      .replace(/\[([^\]]+)]\([^)]+\)/g, "$1")
+      // Markdown画像を削除
+      .replace(/!\[[^\]]*]\([^)]+\)/g, "")
+      // HTMLタグを削除し、タグ内のテキストを保持
+      .replace(/<\/?[^>]+(>|$)/g, "")
+      // 見出しなどの余計なものを削除
+      .replace(/(^|\n)(#+\s|<[^>]+>)/g, "")
+      // 改行をスペースに変換
+      .replace(/\n+/g, " ")
+      // 不要なスペースをトリム
+      .trim()
+
+    return paragraphs.substring(0, EXTRACTED_PARAGRAPHS_LENGTH)
+  }
+
+  /**
+   * 日付文字列をyyyy/MM/dd形式に整形する
+   *
+   * @param dateString - 日付文字列
+   * @returns yyyyMM/dd
+   */
+  const formatDateString = (dateString: string): string => {
+    const date = new Date(dateString)
+    const year = date.getFullYear()
+    const month = `0${date.getMonth() + 1}`.slice(-2)
+    const day = `0${date.getDate()}`.slice(-2)
+    return `${year}/${month}/${day}`
+  }
+
   const thumbnailUrl = `${STRAPI_BASE_URL}${articleInfo.thumbnail.data?.attributes?.url}`
 
   const thumbnailDominantColorRGB = await getColorFromURL(thumbnailUrl)
@@ -111,58 +111,58 @@ export const insertTableOfContents = (markdown: string): string => {
 }
 
 /**
- * childrenからテキストの部分を抽出する関数
- *
- * @param node - ReactNode
- * @returns 文字列に変換されたReactNode
- */
-const extractTextFromChildren = (node: ReactNode): string => {
-  if (typeof node === "string" || typeof node === "number") {
-    return node.toString()
-  }
-
-  if (Array.isArray(node)) {
-    return node.map(extractTextFromChildren).join("")
-  }
-
-  if (isValidElement(node)) {
-    if (
-      isDefined(node.props) &&
-      isDefined(node.props.value) &&
-      typeof node.props.value === "string"
-    ) {
-      return node.props.value
-    }
-
-    if (isDefined(node.props) && isDefined(node.props.children)) {
-      return extractTextFromChildren(node.props.children)
-    }
-
-    return renderToStaticMarkup(node)
-  }
-
-  return ""
-}
-
-/**
- * HTMLタグで囲まれた中身のテキストを抽出する
- *
- * @param html - HTML文字列
- * @returns - 抽出されたテキスト
- */
-const extractTextFromHTMLTag = (html: string): string => {
-  const regex = /<[^>]*>([^<]*)<\/[^>]*>/
-  const match = html.match(regex)
-  return isDefined(match) ? match[1] : ""
-}
-
-/**
  * 見出し用のIDを生成する関数
  *
  * @param text - ReactNode | string
  * @returns ID
  */
 export const generateHeadingId = (text: ReactNode | string): string => {
+  /**
+   * HTMLタグで囲まれた中身のテキストを抽出する
+   *
+   * @param html - HTML文字列
+   * @returns - 抽出されたテキスト
+   */
+  const extractTextFromHTMLTag = (html: string): string => {
+    const regex = /<[^>]*>([^<]*)<\/[^>]*>/
+    const match = html.match(regex)
+    return isDefined(match) ? match[1] : ""
+  }
+
+  /**
+   * childrenからテキストの部分を抽出する
+   *
+   * @param node - ReactNode
+   * @returns 文字列に変換されたReactNode
+   */
+  const extractTextFromChildren = (node: ReactNode): string => {
+    if (typeof node === "string" || typeof node === "number") {
+      return node.toString()
+    }
+
+    if (Array.isArray(node)) {
+      return node.map(extractTextFromChildren).join("")
+    }
+
+    if (isValidElement(node)) {
+      if (
+        isDefined(node.props) &&
+        isDefined(node.props.value) &&
+        typeof node.props.value === "string"
+      ) {
+        return node.props.value
+      }
+
+      if (isDefined(node.props) && isDefined(node.props.children)) {
+        return extractTextFromChildren(node.props.children)
+      }
+
+      return renderToStaticMarkup(node)
+    }
+
+    return ""
+  }
+
   const targetText =
     typeof text === "string" ? text : extractTextFromHTMLTag(extractTextFromChildren(text))
 
