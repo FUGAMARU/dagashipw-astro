fe45737e55bc874ce149bfa776142d66882b09fe
FUGAMARU <gofugashi@gmail.com>
2025-03-09 01:23:44 +0900

    feat: サイドバーのオーバーレイのボタンを押下したらサイドバーの一番下までスクロールするように

--- src/constants/element.ts
+++ src/constants/element.ts
@@ -10,3 +10,6 @@ export const SIDEBAR_ID = "sidebar"
 
 /** サイドバーがスクロール可能な時に表示するオーバーレイのID */
 export const SIDEBAR_SCROLLABLE_OVERLAY_ID = "sidebar-scrollable-overlay"
+
+/** サイドバーを一番下までスクロールするボタンのID */
+export const SIDEBAR_SCROLL_TO_BOTTOM_BUTTON_ID = "sidebar-scroll-to-bottom-button"
