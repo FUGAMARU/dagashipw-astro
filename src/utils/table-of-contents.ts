ef1eef3cd517595de944fc74b5bbc9f7ab3cc5c5
FUGAMARU <gofugashi@gmail.com>
2025-03-20 17:48:13 +0900

    docs: JSDocに記載の内容が重複表現になっていたので修正

--- src/utils/table-of-contents.ts
+++ src/utils/table-of-contents.ts
@@ -98,11 +98,11 @@ const extractPlainTextFromMarkdown = (markdown: string): string => {
 }
 
 /**
- * 記事本文から読了完了目安時間 (分) を計算する
+ * 記事本文から読了目安時間 (分) を計算する
  * 日本人の平均読書速度が1分間に400～600文字程度と言われているので、間を取って500文字/分として計算
  *
  * @param body - 記事本文
- * @returns 読了完了目安時間 (分)
+ * @returns 読了目安時間 (分)
  */
 export const calculateReadingTime = (body: string): number => {
   const words = extractPlainTextFromMarkdown(body).length
