f76fb7999dc1192087c5669476f11a244e679e31
FUGAMARU <gofugashi@gmail.com>
2025-03-22 16:17:25 +0900

    refactor: アクティブな見出しのやり取り方法をカスタムイベントからNanoStoresに移行

--- src/components/templates/TableOfContents.tsx
+++ src/components/templates/TableOfContents.tsx
@@ -1,15 +1,11 @@
 import { useStore } from "@nanostores/react"
-import { Fragment, useEffect, useState } from "react"
+import { Fragment } from "react"
 
 import { DividerHorizontal } from "@/components/parts/common/DividerHorizontal"
 import { SvgLoader } from "@/components/parts/svg/SvgLoader"
 import styles from "@/components/templates/TableOfContents.module.css"
 import { TableOfContentsItem } from "@/components/templates/TableOfContentsItem"
-import {
-  CUSTOM_EVENT_ACTIVE_HEADING_CHANGE,
-  CUSTOM_EVENT_TABLE_OF_CONTENTS_HYDRATION_COMPLETE
-} from "@/constants/event"
-import { useIsSP } from "@/hooks/useIsSP"
+import { activeHeadingAnchorLinkWithInitialValueAtom } from "@/stores/active-heading-anchor-link"
 import { minutesToReadAtom } from "@/stores/minutes-to-read"
 import { tableOfContentsAtom } from "@/stores/table-of-contents"
 
@@ -21,41 +17,10 @@ export const TableOfContents = () => {
   /** 見出し一覧 */
   const tableOfContents = useStore(tableOfContentsAtom)
 
-  /** アクティブな見出しのID  */
-  const [activeHeadingHref, setActiveHeadingHref] = useState("")
-
-  const isSP = useIsSP()
-
-  useEffect(() => {
-    if (isSP) {
-      return // SP表示の場合目次は記事冒頭固定なので何もしなくて良い
-    }
-
-    setActiveHeadingHref(tableOfContents[0].h2.href) // SPの場合は目次を太字にしたくないので早期returnした後のこのタイミングでsetActiveHeadingHrefする
-
-    const hydrationCompleteEvent = new CustomEvent(
-      CUSTOM_EVENT_TABLE_OF_CONTENTS_HYDRATION_COMPLETE
-    )
-    window.dispatchEvent(hydrationCompleteEvent)
-
-    /** アクティブな見出しアイテムが変わったときの処理 */
-    const handleActiveHeadingChange = (event: CustomEvent<string>) => {
-      console.log("処理")
-      setActiveHeadingHref(event.detail)
-    }
-
-    window.addEventListener(
-      CUSTOM_EVENT_ACTIVE_HEADING_CHANGE,
-      handleActiveHeadingChange as EventListener
-    )
-
-    return () => {
-      window.removeEventListener(
-        CUSTOM_EVENT_ACTIVE_HEADING_CHANGE,
-        handleActiveHeadingChange as EventListener
-      )
-    }
-  }, [isSP, tableOfContents])
+  /** アクティブな見出しのアンカーリンク  */
+  const activeHeadingAnchorLinkWithInitialValue = useStore(
+    activeHeadingAnchorLinkWithInitialValueAtom
+  )
 
   return (
     <div className={styles.tableOfContents}>
@@ -64,7 +29,7 @@ export const TableOfContents = () => {
           <Fragment key={content.h2.title}>
             <TableOfContentsItem
               href={content.h2.href}
-              isActive={activeHeadingHref === content.h2.href}
+              isActive={activeHeadingAnchorLinkWithInitialValue === content.h2.href}
               itemNumber={`${h2Index + 1}`}
               title={content.h2.title}
             />
@@ -73,7 +38,7 @@ export const TableOfContents = () => {
                 <div className={styles.headingH3}>
                   <TableOfContentsItem
                     href={h3.href}
-                    isActive={activeHeadingHref === h3.href}
+                    isActive={activeHeadingAnchorLinkWithInitialValue === h3.href}
                     itemNumber={`${h2Index + 1}-${h3Index + 1}`}
                     title={h3.title}
                   />
@@ -82,7 +47,7 @@ export const TableOfContents = () => {
                   <div key={h4.title} className={styles.headingH4}>
                     <TableOfContentsItem
                       href={h4.href}
-                      isActive={activeHeadingHref === h4.href}
+                      isActive={activeHeadingAnchorLinkWithInitialValue === h4.href}
                       itemNumber={`${h2Index + 1}-${h3Index + 1}-${h4Index + 1}`}
                       title={h4.title}
                     />
