42d2d8d1c31974ab16f9dac419936fb19ddcafb8
FUGAMARU <gofugashi@gmail.com>
2025-05-06 23:55:29 +0900

    feat: textareaコンポーネントに現在の入力文字数が表示されるように

--- src/components/parts/input/TextArea.stories.tsx
+++ src/components/parts/input/TextArea.stories.tsx
@@ -5,13 +5,14 @@ import type { Meta, StoryObj } from "@storybook/react"
 const meta: Meta<typeof TextArea> = {
   component: TextArea,
   tags: ["autodocs"],
-  args: { ref: undefined, key: undefined, errorMessage: undefined },
+  args: { ref: undefined, key: undefined, maxLength: undefined, errorMessage: undefined },
   argTypes: {
     ref: {
       control: "select",
       options: ["(instance: T) => void | (() => VoidOrUndefinedOnly)", "React.RefObject<T>"]
     },
     key: { control: "select", options: ["string", "number", "bigint"] },
+    maxLength: { control: "number" },
     errorMessage: { control: "text" }
   }
 }
@@ -21,13 +22,15 @@ type Story = StoryObj<typeof meta>
 
 export const Primary: Story = {
   args: {
-    placeholder: "Placeholder"
+    placeholder: "Placeholder",
+    maxLength: 1000
   }
 }
 
 export const HasErrorMessage: Story = {
   args: {
     placeholder: "Placeholder",
-    errorMessage: "エラーメッセージ"
+    errorMessage: "エラーメッセージ",
+    maxLength: 1000
   }
 }
--- src/components/parts/input/TextArea.tsx
+++ src/components/parts/input/TextArea.tsx
@@ -1,20 +1,45 @@
+import clsx from "clsx"
+import { useState } from "react"
+
 import styles from "@/components/parts/input/TextArea.module.css"
 import { isValidString } from "@/utils"
 
-import type { ComponentProps } from "react"
+import type { ChangeEvent, ComponentProps } from "react"
 
 /** Props */
 type Props = ComponentProps<"textarea"> & {
+  /** 最大入力可能文字数 */
+  maxLength: number
   /** エラーメッセージ */
   errorMessage?: string
 }
 
 /** TextAreaコンポーネント */
-export const TextArea = ({ errorMessage, ...textAreaProps }: Props) => {
+export const TextArea = ({ maxLength, errorMessage, ...textAreaProps }: Props) => {
+  const [currentLength, setCurrentLength] = useState(0)
+
+  /** textareaの入力が変更された時の処理 */
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setCurrentLength(e.target.value.length)
+    textAreaProps.onChange?.(e)
+  }
+
   return (
     <div className={styles.textArea}>
-      <textarea className={styles.input} {...textAreaProps} />
-      {isValidString(errorMessage) && <span className={styles.error}>{errorMessage}</span>}
+      <textarea
+        className={styles.input}
+        maxLength={maxLength}
+        {...textAreaProps}
+        onChange={handleChange}
+      />
+
+      <div className={clsx(styles.bottom, isValidString(errorMessage) && styles.HasError)}>
+        {isValidString(errorMessage) && <span className={styles.error}>{errorMessage}</span>}
+        {/** TODO: maxLengthの8割か9割行ったらcounterの文字色をオレンジにし、最大文字数まで行ったら赤色にする */}
+        <span className={styles.counter}>
+          {currentLength} / {maxLength}
+        </span>
+      </div>
     </div>
   )
 }
