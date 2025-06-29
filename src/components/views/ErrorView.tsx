512711f27319a423da07e4687c90cd38e5410913
FUGAMARU <gofugashi@gmail.com>
2025-06-29 16:05:21 +0900

    feat: 検索結果0件表示時のデザイン改善

--- src/components/views/ErrorView.tsx
+++ src/components/views/ErrorView.tsx
@@ -1,7 +1,10 @@
+import clsx from "clsx"
+
+import { pickSadEmoji } from "@/components/views/ErrorView.helpers"
 import styles from "@/components/views/ErrorView.module.css"
 
-/** Props */
-type Props = {
+/** エラーページ用のProps */
+type ErrorPage = {
   /** エラーコード */
   errorCode: number
   /** 英語のエラーメッセージ */
@@ -10,13 +13,27 @@ type Props = {
   japaneseMessage: string
 }
 
+/** 空ページ用のProps */
+type EmptyPage = {
+  /** 日本語のエラーメッセージ */
+  japaneseMessage: string
+}
+
+/** Props */
+type Props = ErrorPage | EmptyPage
+
 /** エラー表示用ビュー */
-export const ErrorView = ({ errorCode, englishMessage, japaneseMessage }: Props) => {
+export const ErrorView = (props: Props) => {
+  const { japaneseMessage } = props
+  const isErrorPage = "errorCode" in props && "englishMessage" in props
+
   return (
     <div className={styles.errorView}>
-      <h1 className={styles.code}>{errorCode}</h1>
-      <span className={styles.english}>{englishMessage}</span>
-      <span className={styles.japanese}>{japaneseMessage}</span>
+      {isErrorPage && <h1 className={styles.code}>{props.errorCode}</h1>}
+      {isErrorPage && <span className={styles.english}>{props.englishMessage}</span>}
+      <span
+        className={clsx(styles.japanese, !isErrorPage && styles.EmptyPage)}
+      >{`${japaneseMessage}${pickSadEmoji()}`}</span>
     </div>
   )
 }
