9d6cb30680d74bb1b110ca9816d20dbca42deabe
FUGAMARU <gofugashi@gmail.com>
2025-07-03 23:19:50 +0900

    chore: サイトマップ生成機能追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import node from "@astrojs/node"
 import react from "@astrojs/react"
+import sitemap from "@astrojs/sitemap"
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://dagashi.pw",
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   adapter: node({
     mode: "standalone"
   }),
