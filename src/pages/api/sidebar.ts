7312370dd9054301bfab3e9f1e20c55427cd7eed
FUGAMARU <gofugashi@gmail.com>
2025-12-28 23:39:41 +0900

    refactor: アクセス元IPのバージョン判定方法の改善

--- src/pages/api/sidebar.ts
+++ src/pages/api/sidebar.ts
@@ -1,17 +1,22 @@
+import { isValidString } from "@/utils"
+
 import type { SidebarApiResponse } from "@/types/api"
 import type { APIRoute } from "astro"
 
 export const prerender = false
 
 /** サイドバーに表示するデーターをレスポンスする */
-export const GET: APIRoute = ({ clientAddress }) => {
+export const GET: APIRoute = ({ clientAddress, request }) => {
   /** IPアドレスがIPv4アドレスかどうかチェックする */
   const checkIsIPv4Address = (address: string) => {
     // ドットを3つ含むかどうかでIPv4アドレスかどうかを判定
     return address.split(".").length === 4
   }
 
-  const isIPv4Address = checkIsIPv4Address(clientAddress)
+  const forwardedFor = request.headers.get("x-forwarded-for") // Traefik用
+  const clientIpAddress = isValidString(forwardedFor) ? forwardedFor.split(",")[0] : clientAddress
+
+  const isIPv4Address = checkIsIPv4Address(clientIpAddress)
 
   return new Response(
     JSON.stringify({
